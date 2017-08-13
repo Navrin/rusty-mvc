61f987219f8d842f01f540544e34a44a21efca47
Navrin <navman360@gmail.com>
2017-08-13 23:03:00 +1000

    Add send_file method

--- rusty-test/src/main.rs
+++ rusty-test/src/main.rs
@@ -1,41 +1,36 @@
 extern crate rusty_server;
+use std::collections::HashMap;
 use rusty_server::server::Server;
 use rusty_server::router::Router;
 use rusty_server::request::Request;
 use rusty_server::response::Response;
 
+fn param_exists<T: ToString>(params: Option<HashMap<String, String>>, param: T) -> Option<String> {
+    let param = param.to_string();
+    match params {
+        Some(p) => match p.get(&param) {
+            Some(v) => return Some(v.to_string()),
+            None => return None,
+        },
+        None => return None,
+    }
+}
+
 fn main() {
     let mut server = Server::new();
     let mut router = Router::new();
-    let mut second_router = Router::new();
-    router.get("/bep", |req, res| {
-        println!("Hi");
-    });
 
     router.get("/", |req, mut res: Response| {
-        res.send("Hello, this is a test!");
+        res.send_file("./static/index.html");
     });
 
-    router.get("/a", |test, res| {}).post("/ext", |req, res| {});
-
-    router.get("/dogs", |req, res| {
-        println!("WOHOO DOGS");
-    });
-
-    router.get("/cats", |req, res| {
-        println!("Cats are alright too");
-    });
-
-    router.get("/dogs/:id", |req: Request, res| {
-        println!("{}", req.params.unwrap().get("id").unwrap());
-    });
-
-    second_router.get("/a-mystery", |req, res| {
-        println!("Hi!");
+    router.get("/:id", |req: Request, mut res: Response| {
+        match param_exists(req.params, "id") {
+            Some(v) => res.send(format!("Hi!, You requested {}", v)),
+            None => res.send("You didn't request anything!"),
+        };
     });
 
     server.register("/", router);
-    server.register("/inner", second_router);
-
     server.listen(3030, None, None);
 }
--- src/server/response/mod.rs
+++ src/server/response/mod.rs
@@ -1,4 +1,6 @@
 use std::net::TcpStream;
+use std::fs::File;
+use std::path::Path;
 use std::io::Error;
 use std::io::prelude::*;
 use std::collections::HashMap;
@@ -19,6 +21,22 @@ fn status_to_named(status: u16) -> String {
         _ => panic!("Code not implemented! (sorry!)"),
     };
     res.to_string()
+}
+
+pub trait Pathable {
+    fn parse(&self) -> String;
+}
+
+impl<T: ToString> Pathable for T {
+    fn parse(&self) -> String {
+        self.to_string()
+    }
+}
+
+impl Pathable for Path {
+    fn parse(&self) -> String {
+        self.to_string_lossy().to_string()
+    }
 }
 
 pub struct Response {
@@ -44,12 +62,31 @@ impl Response {
         self
     }
 
+    // sets the content type header. allows for easier method chaining like
+    /// `res.content_type("application/json").send(json!{ "hello": "world" })`
+    pub fn content_type<T: ToString>(&mut self, setting: T) -> &mut Response {
+        self.headers.insert("Content-Type".to_string(), setting.to_string());
+        self
+    }
+
     pub fn send<T: ToString>(&mut self, body: T) -> Result<&mut Response, Error> {
         let payload = self.create_response(body.to_string());
 
         self.stream.write_all(payload.as_bytes())?;
 
         Ok(self)
+    }
+
+    /// Sends a file as the response. chain with `.content_type` to set the proper header
+    /// By default, `.` is the project root. eg `src/ Cargo.lock Cargo.toml`. Use a static path for more clarity.
+    pub fn send_file<T: Pathable>(&mut self, path: T) -> Result<&mut Response, Error> {
+        let mut contents = String::new();
+
+        let path = path.parse();
+        let mut file = File::open(path)?;
+        file.read_to_string(&mut contents)?;
+
+        self.send(contents)
     }
 
     fn create_response(&self, body: String) -> String {
